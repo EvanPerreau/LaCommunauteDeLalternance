35d3f0d7145d7f4059e744308fdb5de0db02b170
EvanPerreau <evan.perreau@iut-dijon.u-bourgogne.fr>
2025-04-12 10:04:02 +0200

    feat: Setup CI workflow, IDE config, and add example test
    Adds a CI workflow for building and testing with Gradle. Configures IDE settings for code style and formatting. Adds an example test to demonstrate testing setup. Updates .gitignore.

---
+++ src/test/java/fr/evanperreau/lacommunautedelalternance/ExampleTest.java
@@ -0,0 +1,12 @@
+package fr.evanperreau.lacommunautedelalternance;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
